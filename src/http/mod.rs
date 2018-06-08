5c415821db4d680e706e44f68117f4e5b10ee76b
matrixbirds <yooobuntu@163.com>
2018-06-08 17:27:31 +0800

    proto. add multiple thread

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,3 +1,20 @@
 pub enum HTTP {
-    OK,
+    OK { code: u32, desc: &'static str },
+    NotFound { code: u32, desc: &'static str },
+}
+
+static PROTOCOL: &'static str = "HTTP/1.1";
+
+impl HTTP {
+    fn value(state: HTTP) -> &'static str {
+        match state {
+            OK => "200 OK\r\n\r\n",
+            NotFound => "404 OK\r\n\r\n",
+        }
+    }
+
+    //fn message(state: HTTP) -> &'static str {
+    //    let mut protocol = format!("{}", PROTOCOL);
+    //    format!("{} {} {}", protocol, state.desc, state.code)
+    //}
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -2,6 +2,8 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
 
 pub struct Server<'a> {
     name: &'a str,
@@ -22,7 +24,9 @@ impl<'a> Server<'a> {
         let listener = self.bind();
         for stream in listener.incoming() {
             let stream = stream.unwrap();
-            self.handle_connection(stream);
+            thread::spawn(|| {
+                self.handle_connection(stream);
+            });
         }
     }
 
@@ -34,24 +38,21 @@ impl<'a> Server<'a> {
         let mut buffer = [0; 512];
         stream.read(&mut buffer).unwrap();
         let get = b"GET / HTTP/1.1\r\n";
-        if buffer.starts_with(get) {
-            let mut file = File::open("hello.html").unwrap();
-
-            let mut contents = String::new();
-            file.read_to_string(&mut contents).unwrap();
-            let response = format!("HTTP/1.1 200 OK\r\n\rn{}", contents);
-
-            println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-            stream.write(response.as_bytes()).unwrap();
-            stream.flush().unwrap();
+        let sleep = b"GET /sleep HTTP/1.1\r\n";
+        let (status_line, filename) = if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        } else if buffer.starts_with(sleep) {
+            // thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
         } else {
-            let status_line = "HTTP/1.1 404 NOT FOUND \r\n\r\n";
-            let mut file = File::open("404.html").unwrap();
-            let mut contents = String::new();
-            file.read_to_string(&mut contents).unwrap();
-            let response = format!("{}{}", status_line, contents);
-            stream.write(response.as_bytes()).unwrap();
-            stream.flush().unwrap();
-        }
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        };
+        let mut file = File::open(filename).unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+        let response = format!("{}{}", status_line, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
