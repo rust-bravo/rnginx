7ca419d651e3c4903c2ef1c740ea67ca7155946b
matrixbirds <yooobuntu@163.com>
2018-06-18 16:49:46 +0800

    proto. stdout => stderr

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::process;
 fn main() {
     let server = server::Server::new("baka", "127.0.0.1:23333");
     if let Err(e) = server.boot() {
-        panic!("boot server failure");
+        eprintln!("boot server failure");
         process::exit(0x0100);
     }
 }
